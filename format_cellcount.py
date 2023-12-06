e0b4b5234e611faad712dd74d12ef2d9c2915d1e
Wayne Wu <wayne@WWU-MBP14.local>
2023-12-06 14:22:47 -0800

    append dataframe to existing csv file and move files to archive after processing, removed txt file tracking method

--- format_cellcount.py
+++ format_cellcount.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os, sys
 import psycopg2
 from psycopg2 import Error
+import shutil
 
 # define the path to be base path of the PC + the folder containing the data source
 def resource_path(relative_path):
@@ -13,49 +14,70 @@ def resource_path(relative_path):
     path = os.path.join(base_path, relative_path)
 
     return path
-    
-def cell_count_processing(root_directory):
+
+# since the files have same format and doesnt need to be opened, can move to archive after finishing
+def cell_count_processing(root_directory, archive_directory):
     print(root_directory)
     # Loop through file in directory 
 
     df_list = []
     for files in os.listdir(root_directory):
-        file_path = os.path.join(root_directory, files)
+        
+        # for each files + folder in the listdir, get the extension
+        ext = os.path.splitext(files)[-1].lower()
 
+        # process only csv files
+        if ext == ".csv":
+            file_path = os.path.join(root_directory, files)
         # make a text file if not exist
-        txtfile_path = os.path.join(os.path.dirname(__file__), 'cellcount_processed_files.txt')
-        if not os.path.exists(txtfile_path):
-            with open(txtfile_path, 'w'):
-                pass
-        else:
-            pass
+        # txtfile_path = os.path.join(os.path.dirname(__file__), 'cellcount_processed_files.txt')
+        # if not os.path.exists(txtfile_path):
+        #     with open(txtfile_path, 'w'):
+        #         pass
+        # else:
+        #     pass
 
-        remembered_folders_file = 'cellcount_processed_files.txt'
-        with open(remembered_folders_file, 'r+') as f:
-            if file_path not in f.read():   
+        # remembered_folders_file = 'cellcount_processed_files.txt'
+        # with open(remembered_folders_file, 'r+') as f:
+        #     if file_path not in f.read():   
 
-                try:
-                    df = pd.read_csv(file_path)
-                except:
-                    print('file not found')
-                
-            
+        #         try:
+        #             df = pd.read_csv(file_path)
+        #         except:
+        #             print('file not found')
+            try:
+                df = pd.read_csv(file_path)
                 df = df.iloc[:,0].str.split(':\t', expand=True)
                 df = df.set_index(0).transpose()
                 df_list.append(df)
 
-                #f.write(file_path + ',')  
+                shutil.move(file_path, archive_directory)
+            except:
+                print('cannot read file or already exist in the archive. If duplicate file, it will still get appended to the csv file')
+                continue
             
+    
+            #f.write(file_path + ',')  # -------- instead of writing the path to text, can push the file to archive folder  
+        
     try:
         # drops the column and reset with another index, renamed to 'id'
-        combined_df = pd.concat(df_list)[df.columns].reset_index(drop=True).rename_axis('id')
+        # combined_df = pd.concat(df_list)[df.columns].reset_index(drop=True).rename_axis('id')
+        combined_df = pd.concat(df_list)[df.columns].reset_index(drop=True)
+        
+        # append to existing csv file, except the header
+        combined_df.to_csv("/Users/wayne/Documents/Programming/vscode/API/SQL_query/nucleocounter_raw_csv/compiled_cell_count/cell_count.csv", mode='a', header=False, index=False)
+
         #print(combined_df)
         #combined_df.to_csv('test.csv')
     except:
         print('no new files')
 
+
+
     return combined_df
 
+
+# need to edit 12/5/2023
 def insert_cellcount_csv_to_pg(combined_df, table_name):
     df = combined_df
     try:
@@ -133,4 +155,6 @@ def insert_cellcount_csv_to_pg(combined_df, table_name):
 
 
 if __name__ == "__main__":
-    path = resource_path('nucleocounter_raw_csv')
+    process_folder_path = resource_path('nucleocounter_raw_csv')
+    archive_folder_path = resource_path('nucleocounter_raw_csv/archive')
+    cell_count_processing(process_folder_path, archive_folder_path)
